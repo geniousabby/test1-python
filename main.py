9657bf88fb64478363abc8a06a7d770728a2f788
geniousabby <abbyspam1228@gmail.com>
2026-04-23 10:45:59 -0700

    Get name and area function
    Made a function to get robot name and delivery area.

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ Robot Delivery game
 
 
 # Get 3 robot names
+def name_area():
+    """Gets name and area"""
+    n1 = input("Enter robot name: ").strip().title()
+    a1 = input("Choose a delivery zone for Apex (Downtown, Suburbs, Industrial): ").strip().title()
+
+    n2 = input("Enter robot name: ").strip().title()
+    a2 = input("Choose a delivery zone for Apex (Downtown, Suburbs, Industrial): ").strip().title()
+
+    n3 = input("Enter robot name: ").strip().title()
+    a3 = input("Choose a delivery zone for Apex (Downtown, Suburbs, Industrial): ").strip().title()
 
 
 # Assign areas using a dictionary

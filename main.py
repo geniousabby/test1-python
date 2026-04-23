d748fb12d5c95cce53e83f1ce59b540ed6872995
geniousabby <abbyspam1228@gmail.com>
2026-04-23 10:35:56 -0700

    Pseudo code
    Wrote pseudo code to outline what I need to do.

---
+++ main.py
@@ -0,0 +1,23 @@
+
+"""
+Robot Delivery game
+
+"""
+
+
+# Get 3 robot names
+
+
+# Assign areas using a dictionary
+
+
+# Get total delivery distance (1-500km)
+
+
+# Get individual cargo weight (1-50kg)
+
+
+# Get weather condition
+
+
+# Deployment status (safe or unsafe)
